2084fe9500a214c895ec66a12dc2d883cc85c270
lleroc <llleroc@yahoo.es>
2020-07-06 16:14:55 -0500

    Inicial

---
+++ ProyectoGit/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProyectoGit.Models;
+
+namespace ProyectoGit.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<Usuarios>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<IdentityRole> Roles { get; set; }
+    }
+}
---
+++ ProyectoGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
