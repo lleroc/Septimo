119b0b70a33c0597133d207775ff7e20bdd8b208
daltonAriel <dalton_vargas@hotmail.com>
2020-07-06 17:19:04 -0500

    Repuestos
    clase repuestos

---
+++ ProyectoGit/Models/Repuestos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoGit.Models
+{
+    public class Repuestos
+    {
+        public int RepuestosId { get; set; }
+        public string Serie { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
