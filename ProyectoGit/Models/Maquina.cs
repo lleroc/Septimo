e9317567875650472c9c5b2d677c91dedd1c5e3b
ShiriMasaquiza <67808783+ShiriMasaquiza@users.noreply.github.com>
2020-07-06 17:09:15 -0500

    maquina

---
+++ ProyectoGit/Models/Maquina.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoGit.Models
+{
+    public class Maquina
+    {
+
+        public int MaquinaId { get; set; }
+        public string nombre_maquina { get; set; }
+
+        public string serie { get; set; }
+
+        public string descripcion { get; set; }
+
+        public string fecha_ingreso { get; set; }
+
+    }
+}
